1b74cad8dd50359537ad3c8610e7831fae00f2f5
OduorAlphonce <alphonceoyunga@outlook.com>
2026-08-22 06:33:18 +0300

    add path to the frontend in cors

--- backend/internal/api/cors.go
+++ backend/internal/api/cors.go
@@ -43,6 +43,7 @@ func getAllowedOrigins() []string {
 	}
 
 	return []string{
+		"https://situoc-step.netlify.app/"
 		"http://localhost:5173",
 		"http://localhost:3000",
 		"http://localhost:8080",
